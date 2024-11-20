878a7473365bd63bc87960d4431778061d2a3bde
notrhy <cacingfz@gmail.com>
2024-11-20 13:34:49 +0700

    Create 2.8.c

---
+++ 2.8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main () {
+  int i, batas, jumlah;
+
+  printf ("Menjumlahkan 1 + 2 + 3 + ...\n");
+  printf ("Sampai berapa?\n");
+  scanf ("%d", &batas);
+  
+  jumlah = 0;
+  i = 1;
+  while (i < (batas + 1)) {
+    jumlah = jumlah + i;
+    i = i + 1;
+   }
+   printf ("\n 1+2+3+...+%d=%d", batas, jumlah);
+}
