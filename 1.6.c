4c9f38ce670c39d1be4906daac58faa38759b92d
notrhy <cacingfz@gmail.com>
2024-11-20 12:40:01 +0700

    Create 1.6.c

---
+++ 1.6.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main () {
+   int i=8, hasil1;
+   float hasil2,f=6.9;
+   hasil1 = f*i;
+   hasil2 = (int)f*i;
+   printf ("hasil1 : %d \n", hasil1);
+   printf ("hasil2 : %f \n", hasil2);
+}
